88045e94ce6e362c107e04289be78ba8f99ff7c0
kip2 <tqa1no4@gmail.com>
2024-07-03 21:04:53 +0900

    Implement provisional operation for fetching JSON data

--- seeder/src/main.rs
+++ seeder/src/main.rs
@@ -1,3 +1,15 @@
+use std::{fs::File, io::Read};
+
+use serde_json::{Error, Value};
+
 fn main() {
+    let mut file = File::open("data.json").expect("File not found");
+
+    let mut data = String::new();
+    file.read_to_string(&mut data).expect("Failed to read file");
     println!("Hello, world!");
+
+    let v: Value = serde_json::from_str(&data).unwrap();
+
+    println!("{:?}", v);
 }
