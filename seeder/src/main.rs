f68c3fb20a636f2bce27d838a592b96eff14a4cc
kip2 <tqa1no4@gmail.com>
2024-07-03 21:24:21 +0900

    Add process to store fetchec JSON into separate variables

--- seeder/src/main.rs
+++ seeder/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::Read};
 
-use serde_json::{Error, Value};
+use serde_json::Value;
 
 fn main() {
     let mut file = File::open("data.json").expect("File not found");
@@ -11,5 +11,29 @@ fn main() {
 
     let v: Value = serde_json::from_str(&data).unwrap();
 
-    println!("{:?}", v);
+    let mut table_columns: Option<Value> = None;
+    let mut table_row: Option<Value> = None;
+
+    if let Value::Object(map) = v {
+        if let Some(table_columns_value) = map.get("table_columns") {
+            table_columns = Some(table_columns_value.clone());
+        } else {
+            println!("table_columns not found");
+        }
+
+        if let Some(table_row_value) = map.get("table_row") {
+            table_row = Some(table_row_value.clone());
+        } else {
+            println!("table_row not found");
+        }
+    } else {
+        println!("Expected a JSON object");
+    }
+
+    if let Some(tc) = &table_columns {
+        println!("table_columns: {:?}", tc);
+    }
+    if let Some(tr) = &table_row {
+        println!("table_row: {:?}", tr);
+    }
 }
