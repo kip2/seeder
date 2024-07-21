6073cbb7af7ecbdbaafe7240c01e1901226b3c44
kip2 <tqa1no4@gmail.com>
2024-07-21 21:42:25 +0900

    Extracted the insert process into an external function

--- seeder/src/db.rs
+++ seeder/src/db.rs
@@ -1,8 +1,30 @@
-use crate::json::{self, JsonData};
-use chrono::NaiveDate;
+use crate::json::*;
 use dotenv::dotenv;
 use serde_json::Value;
 use sqlx::{PgPool, Transaction};
+use std::{env, error::Error};
+
+pub async fn insert() -> Result<(), Box<dyn Error>> {
+    dotenv().expect("Failed to read .env file");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPool::connect(&database_url).await.unwrap();
+
+    let file_path = "data.json";
+    let data = read_json_file(file_path).unwrap();
+
+    let mut transaction = pool.begin().await.unwrap();
+
+    match insert_data(&mut transaction, data).await {
+        Ok(_) => {
+            transaction.commit().await.unwrap();
+        }
+        Err(e) => {
+            transaction.rollback().await.unwrap();
+            return Err(Box::new(e) as Box<dyn Error>);
+        }
+    };
+    Ok(())
+}
 
 pub async fn insert_data<'a>(
     transaction: &mut Transaction<'a, sqlx::Postgres>,
@@ -70,68 +92,3 @@ pub async fn insert_data<'a>(
 
     Ok(())
 }
-
-// pub async fn insert_row() {
-//     let db = db_pool().await.unwrap();
-
-//     // todo: テーブルネームをJSONデータから取得する関数に置き換える
-//     let table_name = "table_name";
-
-//     let (table_columns, table_rows) = json::read_json_file("data.json");
-//     let (table_columns, table_rows) = tuple_to_string_vector(&table_columns, &table_rows);
-
-//     insert(&db, table_name, (table_columns, table_rows)).await;
-// }
-
-// async fn db_pool() -> Result<AnyPool, Box<dyn Error>> {
-//     dotenv().expect("Fialed to read .env file");
-//     let database_url = env::var("DATABASE_URL").expect("DABASE_URL must be set");
-
-//     install_default_drivers();
-
-//     let pool = AnyPool::connect(&database_url).await?;
-//     Ok(pool)
-// }
-
-// fn generate_placeholder(
-//     database_url: &str,
-//     num_placeholders: usize,
-// ) -> Result<String, Box<dyn Error>> {
-//     match database_url {
-//         url if url.starts_with("postgres://") => Ok((1..=num_placeholders)
-//             .map(|i| format!("${}", i))
-//             .collect::<Vec<String>>()
-//             .join(", ")),
-//         url if url.starts_with("mysql://") => Ok(std::iter::repeat("?")
-//             .take(num_placeholders)
-//             .collect::<Vec<&str>>()
-//             .join(", ")),
-//         _ => Err("Unsupported database URL".into()),
-//     }
-// }
-
-// pub async fn insert(
-//     db: &AnyPool,
-//     table_name: &str,
-//     (table_columns, table_rows): (Vec<String>, Vec<String>),
-// ) -> Result<AnyQueryResult, Box<dyn Error>> {
-//     let database_url = env::var("DATABASE_URL").expect("DATBASE_URL must be set");
-
-//     let placeholder = generate_placeholder(&database_url, table_columns.len()).unwrap();
-
-//     let query = format!(
-//         "INSERT INTO {} ({}) VALUES ({})",
-//         table_name,
-//         table_columns.join(", "),
-//         placeholder
-//     );
-
-//     let mut query_builder = sqlx::query(&query);
-
-//     for value in table_rows {
-//         query_builder = query_builder.bind(value);
-//     }
-
-//     let result = query_builder.execute(db).await;
-//     result.map_err(|e| e.into())
-// }
--- seeder/src/main.rs
+++ seeder/src/main.rs
@@ -1,31 +1,6 @@
-use std::{env, error::Error};
-
-use dotenv::dotenv;
-use seeder::{
-    db::{self, insert_data},
-    json::*,
-};
-use sqlx::PgPool;
+use seeder::db::insert;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    dotenv().expect("Failed to read .env file");
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = PgPool::connect(&database_url).await.unwrap();
-
-    let file_path = "data.json";
-    let data = read_json_file(file_path).unwrap();
-
-    let mut transaction = pool.begin().await.unwrap();
-
-    match insert_data(&mut transaction, data).await {
-        Ok(_) => {
-            transaction.commit().await.unwrap();
-        }
-        Err(e) => {
-            transaction.rollback().await.unwrap();
-            return Err(Box::new(e) as Box<dyn Error>);
-        }
-    };
-    Ok(())
+async fn main() {
+    insert().await.expect("Failed INSERT query");
 }
