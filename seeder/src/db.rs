95dbf1961e886ca50e7e596c0a533d74dd17b74b
kip2 <tqa1no4@gmail.com>
2024-07-21 20:18:10 +0900

    Change the method for reading JSON files

--- seeder/src/db.rs
+++ seeder/src/db.rs
@@ -4,69 +4,69 @@ use sqlx::any::{install_default_drivers, AnyQueryResult};
 use sqlx::{database, query, AnyPool, Row};
 use std::{env, error::Error, fs};
 
-use crate::json::{self, tuple_to_string_vector};
+use crate::json::{self};
 
-pub async fn insert_row() {
-    let db = db_pool().await.unwrap();
+// pub async fn insert_row() {
+//     let db = db_pool().await.unwrap();
 
-    // todo: テーブルネームをJSONデータから取得する関数に置き換える
-    let table_name = "table_name";
+//     // todo: テーブルネームをJSONデータから取得する関数に置き換える
+//     let table_name = "table_name";
 
-    let (table_columns, table_rows) = json::read_json_file("data.json");
-    let (table_columns, table_rows) = tuple_to_string_vector(&table_columns, &table_rows);
+//     let (table_columns, table_rows) = json::read_json_file("data.json");
+//     let (table_columns, table_rows) = tuple_to_string_vector(&table_columns, &table_rows);
 
-    insert(&db, table_name, (table_columns, table_rows)).await;
-}
+//     insert(&db, table_name, (table_columns, table_rows)).await;
+// }
 
-async fn db_pool() -> Result<AnyPool, Box<dyn Error>> {
-    dotenv().expect("Fialed to read .env file");
-    let database_url = env::var("DATABASE_URL").expect("DABASE_URL must be set");
+// async fn db_pool() -> Result<AnyPool, Box<dyn Error>> {
+//     dotenv().expect("Fialed to read .env file");
+//     let database_url = env::var("DATABASE_URL").expect("DABASE_URL must be set");
 
-    install_default_drivers();
+//     install_default_drivers();
 
-    let pool = AnyPool::connect(&database_url).await?;
-    Ok(pool)
-}
+//     let pool = AnyPool::connect(&database_url).await?;
+//     Ok(pool)
+// }
 
-fn generate_placeholder(
-    database_url: &str,
-    num_placeholders: usize,
-) -> Result<String, Box<dyn Error>> {
-    match database_url {
-        url if url.starts_with("postgres://") => Ok((1..=num_placeholders)
-            .map(|i| format!("${}", i))
-            .collect::<Vec<String>>()
-            .join(", ")),
-        url if url.starts_with("mysql://") => Ok(std::iter::repeat("?")
-            .take(num_placeholders)
-            .collect::<Vec<&str>>()
-            .join(", ")),
-        _ => Err("Unsupported database URL".into()),
-    }
-}
+// fn generate_placeholder(
+//     database_url: &str,
+//     num_placeholders: usize,
+// ) -> Result<String, Box<dyn Error>> {
+//     match database_url {
+//         url if url.starts_with("postgres://") => Ok((1..=num_placeholders)
+//             .map(|i| format!("${}", i))
+//             .collect::<Vec<String>>()
+//             .join(", ")),
+//         url if url.starts_with("mysql://") => Ok(std::iter::repeat("?")
+//             .take(num_placeholders)
+//             .collect::<Vec<&str>>()
+//             .join(", ")),
+//         _ => Err("Unsupported database URL".into()),
+//     }
+// }
 
-pub async fn insert(
-    db: &AnyPool,
-    table_name: &str,
-    (table_columns, table_rows): (Vec<String>, Vec<String>),
-) -> Result<AnyQueryResult, Box<dyn Error>> {
-    let database_url = env::var("DATABASE_URL").expect("DATBASE_URL must be set");
+// pub async fn insert(
+//     db: &AnyPool,
+//     table_name: &str,
+//     (table_columns, table_rows): (Vec<String>, Vec<String>),
+// ) -> Result<AnyQueryResult, Box<dyn Error>> {
+//     let database_url = env::var("DATABASE_URL").expect("DATBASE_URL must be set");
 
-    let placeholder = generate_placeholder(&database_url, table_columns.len()).unwrap();
+//     let placeholder = generate_placeholder(&database_url, table_columns.len()).unwrap();
 
-    let query = format!(
-        "INSERT INTO {} ({}) VALUES ({})",
-        table_name,
-        table_columns.join(", "),
-        placeholder
-    );
+//     let query = format!(
+//         "INSERT INTO {} ({}) VALUES ({})",
+//         table_name,
+//         table_columns.join(", "),
+//         placeholder
+//     );
 
-    let mut query_builder = sqlx::query(&query);
+//     let mut query_builder = sqlx::query(&query);
 
-    for value in table_rows {
-        query_builder = query_builder.bind(value);
-    }
+//     for value in table_rows {
+//         query_builder = query_builder.bind(value);
+//     }
 
-    let result = query_builder.execute(db).await;
-    result.map_err(|e| e.into())
-}
+//     let result = query_builder.execute(db).await;
+//     result.map_err(|e| e.into())
+// }
--- seeder/src/json.rs
+++ seeder/src/json.rs
@@ -1,5 +1,23 @@
-use serde_json::Value;
-use std::{fs::File, io::Read};
+use serde::Deserialize;
+use serde_json::{json, Value};
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufReader, Read},
+};
+
+#[derive(Deserialize, Debug, PartialEq)]
+struct TableColumn {
+    data_type: String,
+    column_name: String,
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+struct JsonData {
+    table_name: String,
+    table_columns: Vec<TableColumn>,
+    table_rows: Vec<Vec<Value>>,
+}
 
 /// 渡したjsonファイルについてのバリデーションをまとめて行う関数
 ///
@@ -10,48 +28,19 @@ use std::{fs::File, io::Read};
 ///
 /// jsonファイルのパス
 ///
-pub fn validate_json_data(json_file_path: &str) -> bool {
-    let (table_columns, table_row) = read_json_file(json_file_path);
+// pub fn validate_json_data(json_file_path: &str) -> bool {
+//     let (table_columns, table_row) = read_json_file(json_file_path);
 
-    if !validate_row_column_length(&table_columns, &table_row) {
-        return false;
-    };
+//     if !validate_row_column_length(&table_columns, &table_row) {
+//         return false;
+//     };
 
-    if !validate_table_columns_type(&table_columns) {
-        return false;
-    };
+//     if !validate_table_columns_type(&table_columns) {
+//         return false;
+//     };
 
-    true
-}
-
-/// JSONから読み込んだタプルを、Vec<String>に変換する関数
-///
-/// # Arguments
-/// ```
-/// table_columns: &Option<Value>, table_row: &Option<Value>
-/// ```
-///
-/// # Return
-/// ```
-/// table_columns: Vec<String>, table_row: Vec<String>
-/// ```
-///
-pub fn tuple_to_string_vector(
-    table_columns: &Option<Value>,
-    table_row: &Option<Value>,
-) -> (Vec<String>, Vec<String>) {
-    let column_vec = match table_columns {
-        Some(value) => vec![value.to_string()],
-        None => vec!["None".to_string()],
-    };
-
-    let row_vec = match table_row {
-        Some(value) => vec![value.to_string()],
-        None => vec!["None".to_string()],
-    };
-
-    (column_vec, row_vec)
-}
+//     true
+// }
 
 /// カラムのデータタイプが全て、使用して良い型かどうかを判定する
 ///
@@ -76,65 +65,65 @@ pub fn tuple_to_string_vector(
 /// カラムデータに使用を許容されていないデータが入っていないかどうかを判定する
 ///
 ///
-pub fn validate_table_columns_type(table_columns: &Option<Value>) -> bool {
-    let variable_types = ["int", "string", "float"];
+// pub fn validate_table_columns_type(table_columns: &Option<Value>) -> bool {
+//     let variable_types = ["int", "string", "float"];
 
-    let table_columns_value = table_columns.as_ref().unwrap();
-    let columns_data = table_columns_value.as_array().unwrap();
+//     let table_columns_value = table_columns.as_ref().unwrap();
+//     let columns_data = table_columns_value.as_array().unwrap();
 
-    for data in columns_data {
-        let data_type_str = data.get("data_type").unwrap().as_str().unwrap();
-        if !variable_types.contains(&data_type_str) {
-            return false;
-        }
-    }
-    true
-}
+//     for data in columns_data {
+//         let data_type_str = data.get("data_type").unwrap().as_str().unwrap();
+//         if !variable_types.contains(&data_type_str) {
+//             return false;
+//         }
+//     }
+//     true
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use serde_json::json;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use serde_json::json;
 
-    #[test]
-    fn test_validate_table_columns_type_valid() {
-        let table_columns = json!([
-            {"column_name":"name", "data_type":"string"},
-            {"column_name":"age", "data_type":"int"},
-            {"column_name":"salary", "data_type":"float"}
-        ]);
-        assert!(validate_table_columns_type(&Some(table_columns)));
-    }
+//     #[test]
+//     fn test_validate_table_columns_type_valid() {
+//         let table_columns = json!([
+//             {"column_name":"name", "data_type":"string"},
+//             {"column_name":"age", "data_type":"int"},
+//             {"column_name":"salary", "data_type":"float"}
+//         ]);
+//         assert!(validate_table_columns_type(&Some(table_columns)));
+//     }
 
-    #[test]
-    fn test_validate_table_columns_type_invalid_type() {
-        let table_columns = json!([
-            {"column_name":"name", "data_type":"string"},
-            // invalid type "integer"
-            {"column_name":"age", "data_type":"integer"},
-            {"column_name":"salary", "data_type":"float"}
-        ]);
-        assert!(!validate_table_columns_type(&Some(table_columns)));
-    }
+//     #[test]
+//     fn test_validate_table_columns_type_invalid_type() {
+//         let table_columns = json!([
+//             {"column_name":"name", "data_type":"string"},
+//             // invalid type "integer"
+//             {"column_name":"age", "data_type":"integer"},
+//             {"column_name":"salary", "data_type":"float"}
+//         ]);
+//         assert!(!validate_table_columns_type(&Some(table_columns)));
+//     }
 
-    #[test]
-    fn test_validate_table_columns_type_missing_data_type() {
-        let table_columns = json!([
-            {"column_name":"name", "data_type":"string"},
-            // missing data type
-            {"column_name":"age"},
-            {"column_name":"salary", "data_type":"float"}
-        ]);
-        let result = std::panic::catch_unwind(|| validate_table_columns_type(&Some(table_columns)));
-        assert!(result.is_err());
-    }
+//     #[test]
+//     fn test_validate_table_columns_type_missing_data_type() {
+//         let table_columns = json!([
+//             {"column_name":"name", "data_type":"string"},
+//             // missing data type
+//             {"column_name":"age"},
+//             {"column_name":"salary", "data_type":"float"}
+//         ]);
+//         let result = std::panic::catch_unwind(|| validate_table_columns_type(&Some(table_columns)));
+//         assert!(result.is_err());
+//     }
 
-    #[test]
-    fn test_validate_table_columns_type_empty_array() {
-        let table_columns = json!([]);
-        assert!(validate_table_columns_type(&Some(table_columns)));
-    }
-}
+//     #[test]
+//     fn test_validate_table_columns_type_empty_array() {
+//         let table_columns = json!([]);
+//         assert!(validate_table_columns_type(&Some(table_columns)));
+//     }
+// }
 
 /// カラムデータの個数と、ロウデータの個数が一致しているかをバリデーションする関数
 ///
@@ -158,78 +147,101 @@ mod tests {
 /// bool
 /// ```
 ///
-pub fn validate_row_column_length(
-    table_columns: &Option<Value>,
-    table_row: &Option<Value>,
-) -> bool {
-    let table_columns_value = table_columns.as_ref().expect("table_columns not found");
+// pub fn validate_row_column_length(
+//     table_columns: &Option<Value>,
+//     table_row: &Option<Value>,
+// ) -> bool {
+//     let table_columns_value = table_columns.as_ref().expect("table_columns not found");
 
-    let columns = table_columns_value
-        .as_array()
-        .expect("table_columns is not a valid array");
+//     let columns = table_columns_value
+//         .as_array()
+//         .expect("table_columns is not a valid array");
 
-    let columns_len = columns.len();
+//     let columns_len = columns.len();
 
-    let table_row_values = table_row.as_ref().expect("table_row not found");
+//     let table_row_values = table_row.as_ref().expect("table_row not found");
 
-    let rows = table_row_values
-        .as_array()
-        .expect("table_row is not a valid array");
+//     let rows = table_row_values
+//         .as_array()
+//         .expect("table_row is not a valid array");
 
-    let all_rows_have_same_length = rows
-        .iter()
-        .all(|row| row.as_array().expect("Each row should be an array").len() == columns_len);
+//     let all_rows_have_same_length = rows
+//         .iter()
+//         .all(|row| row.as_array().expect("Each row should be an array").len() == columns_len);
 
-    if all_rows_have_same_length {
-        true
-    } else {
-        false
-    }
-}
+//     if all_rows_have_same_length {
+//         true
+//     } else {
+//         false
+//     }
+// }
 
 /// JSONファイルに設定されたインサート用のSQLデータを読み込む関数
 ///
-/// # Arguments
-/// ```
-/// file_path: &str
-/// ```
-/// 読み込む対象のJSONファイルパス
+/// JSONファイルのデータ構造は以下の通りである
 ///
-/// # Returns
-///
+/// ```json
+/// {
+///     "table_columns": [
+///         {
+///             "data_type": "string",
+///             "column_name": "name",
+///         },
+///         {
+///         "data_type": "int",
+///         "column_name": "lifespan"
+///         }
+///     ]
+///     ,
+///     "table_rows": [
+///         [
+///         "Ryzen 9 5900X",
+///         5
+///         ],
+///     ]
+/// }
 /// ```
-/// (table_columns: Option<Value>, table_row: Option<Value>)
-/// ```
 ///
-/// JSONファイルのカラムデータのタプル。
-/// JSONファイルから取得したテーブルカラムとテーブルロウの2つをタプルにして返す
-/// 型はserde::json::Value
-///
-pub fn read_json_file(file_path: &str) -> (Option<Value>, Option<Value>) {
+pub fn read_json_file(file_path: &str) -> Result<JsonData, Box<dyn Error>> {
     let mut file = File::open(file_path).expect("File not found");
-    let mut data = String::new();
-    file.read_to_string(&mut data).expect("Failed to read file");
+    let reader = BufReader::new(file);
+    let data = serde_json::from_reader(reader).expect("Failed JSON file.");
 
-    let v: Value = serde_json::from_str(&data).unwrap();
+    Ok(data)
+}
 
-    let mut table_columns: Option<Value> = None;
-    let mut table_row: Option<Value> = None;
+#[test]
+fn test_read_json_file() {
+    let file_path = "test/test.json";
 
-    if let Value::Object(map) = v {
-        if let Some(table_columns_value) = map.get("table_columns") {
-            table_columns = Some(table_columns_value.clone());
-        } else {
-            println!("table_columns not found");
-        }
+    let expected_data = JsonData {
+        table_name: "test_table".to_string(),
+        table_columns: vec![
+            TableColumn {
+                data_type: "string".to_string(),
+                column_name: "name".to_string(),
+            },
+            TableColumn {
+                data_type: "string".to_string(),
+                column_name: "type".to_string(),
+            },
+            TableColumn {
+                data_type: "string".to_string(),
+                column_name: "brand".to_string(),
+            },
+        ],
+        table_rows: vec![
+            vec![json!("Ryzen 9 5900X"), json!("CPU"), json!("AMD")],
+            vec![json!("GeForce RTX 3080"), json!("GPU"), json!("NVIDIA")],
+            vec![
+                json!("Samusung 970 EVO SSD"),
+                json!("SSD"),
+                json!("Samsung"),
+            ],
+        ],
+    };
 
-        if let Some(table_row_value) = map.get("table_row") {
-            table_row = Some(table_row_value.clone());
-        } else {
-            println!("table_row not found");
-        }
-    } else {
-        println!("Expected a JSON object");
-    }
+    let result = read_json_file(&file_path).unwrap();
 
-    (table_columns, table_row)
+    assert_eq!(result, expected_data);
 }
