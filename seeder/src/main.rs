8058347f91d2df21e9e756726973c11b5738a7fa
kip2 <tqa1no4@gmail.com>
2024-07-27 22:03:09 +0900

    Delete unnecessary comment

--- seeder/src/main.rs
+++ seeder/src/main.rs
@@ -1,19 +1,8 @@
 use seeder::console::run;
-use seeder::db::insert_random_data;
-use seeder::json::generate_random_data;
 use std::process;
 
 #[tokio::main]
 async fn main() {
-    let n = 2;
-    // let data = generate_random_data("column.json", n);
-    // println!("{}", serde_json::to_string_pretty(&data).unwrap());
-
-    // if let Err(e) = insert_random_data(&"column.json", n).await {
-    //     eprintln!("Error: {}", e);
-    //     process::exit(1);
-    // }
-
     if let Err(e) = run().await {
         eprintln!("Error: {}", e);
         process::exit(1);
